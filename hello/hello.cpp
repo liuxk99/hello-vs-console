614272b0903de733c2dc6c5b7f7d15241e80f9ba
Thomas Liu <liuxk99@gmail.com>
2021-06-25 13:56:28 +0800

    <CM>Init hello console project files.

---
+++ hello/hello.cpp
@@ -0,0 +1,13 @@
+// hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world!" << endl;
+    return 0;
+}
